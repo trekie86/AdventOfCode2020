e3608f176577fff298851131214b1e7069e37d04
Rob Wolinski <trekie86@gmail.com>
2020-12-02 22:58:25 -0700

    address some pylint findings

--- day3/part_2.py
+++ day3/part_2.py
@@ -1,36 +1,34 @@
-"""Advent of Code 2020 day 3 part 1"""
+"""Advent of Code 2020 day 3 part 2"""
 
-from numpy import *
 from math import prod
 
 def find_trees(move_h: int, move_v: int) -> int:
-    map = []
+    """Reads in a two dimensional array from file and walks the matrix to find trees"""
+    matrix = []
 
     with open("day3/input.txt", "r") as f:
         for line in f.readlines():
-            map.append(list(line.strip()))
+            matrix.append(list(line.strip()))
     trees = 0
     current_pos_h = 0
     current_pos_v = 0
 
     while True:
-        if(current_pos_h + move_h > len(map[current_pos_v])-1):
-            current_pos_h += move_h - len(map[current_pos_v])
+        if current_pos_h + move_h > len(matrix[current_pos_v])-1:
+            current_pos_h += move_h - len(matrix[current_pos_v])
         else:
             current_pos_h += move_h
 
-        if(current_pos_v + move_v > len(map)-1):
+        if current_pos_v + move_v > len(matrix)-1:
             break
         else:
             current_pos_v += move_v
 
         # print(f"Checking position h: {current_pos_h} & v: {current_pos_v}")
-        if(map[current_pos_v][current_pos_h] == '#'):
+        if matrix[current_pos_v][current_pos_h] == '#':
             trees += 1
 
     return trees
-    print(map)
-
 
 results = []
 results.append(find_trees(1,1))
